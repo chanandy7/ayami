075ef96eca3a0754675101282be4e7050d5552d9
Andy Chan <andy.chan7@hotmail.co.uk>
2022-06-26 12:02:20 +1200

    design

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+
+
+
+
+    return (
+        <div className="App">
+            <style> {'body { background-color: black;}'}</style>
+            <h1 className="vertical-rl" >{`痴漢です`}<br /><div className='vertical-rl2 '>助けてください</div> </h1>
+            <h2 className="tapMessage">画面をタップすると作動します</h2>
+            <h3 className="silentModeMessage">マナーモードでも音がなりますので、使用の際はご注意ください</h3>
+
+
+        </div>
+    );
+}
+
+export default App;
